04b90e5263537ec3b7bcb4d2f4b5a7710ca0e477
MariaJulia <mariajuliadev@gmail.com>
2026-01-09 16:05:19 -0300

    feat: install cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response, NextFunction } from "express";
 import { router } from "./routes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use(cors());
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
